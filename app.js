c086e47549a2b77017b34cd90459a1660476d03c
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-08-10 09:14:48 -0430

    Desplegando aplicación con react

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
 require('dotenv').config()
 const express = require('express')
-const hbs = require('hbs')
 
 const app = express()
 const port = process.env.PORT
 
-//Handlebars
-app.set('view engine', 'hbs')
-hbs.registerPartials(__dirname + '/views/partials')
-
-//Servir contenido estático a través de middleware
-app.use(express.static('public'))
-
-app.get('/', function (req, res) {
-  res.render('home', {
-  	nombre: 'Mariangel',
-  	titulo: 'Webserver con Node'
-  })
-})
-
-app.get('/generic', function (req, res) {
-  res.render('generic', {
-  	nombre: 'Mariangel',
-  	titulo: 'Webserver con Node'
-  })
-})
-
-app.get('/elements', function (req, res) {
-  res.render('elements', {
-  	nombre: 'Mariangel',
-  	titulo: 'Webserver con Node'
-  })
-})
-
 app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/public/404.html')
+  res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(port, () => {
